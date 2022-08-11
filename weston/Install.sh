c68b721f007c2c3a04e59c07a04927a7cfd83db6
Anupam Kaul <anupam.kaul@yahoo.com>
2022-08-11 14:19:03 -0700

    Add $WLD/lib/pkgconfig to $PATH_PKGCONFIG to recognize libcheck (.pc) files
    I had to install libcheck in order to compile libinput

--- weston/Install.sh
+++ weston/Install.sh
@@ -2,7 +2,7 @@
 export WLROOT=$HOME/Wayland
 export WLD=$WLROOT/install   # change this to another location if you prefer
 export LD_LIBRARY_PATH=$WLD/lib
-export PKG_CONFIG_PATH=$WLD/lib/x86_64-linux-gnu/pkgconfig/:$WLD/share/x86_64-linux-gnu/pkgconfig/:$WLD/share/pkgconfig
+export PKG_CONFIG_PATH=$WLD/lib/x86_64-linux-gnu/pkgconfig/:$WLD/share/x86_64-linux-gnu/pkgconfig/:$WLD/share/pkgconfig:$WLD/lib/pkgconfig
 export PATH=$WLD/bin:$PATH
 
 # execute this as 'source ~/.Install.sh'
